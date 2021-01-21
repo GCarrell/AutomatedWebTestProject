49ef6e6e2e44a3ce0300b98d97ff102bb45210c7
Beanca <serdenabeanca@gmail.com>
2021-01-21 12:24:54 +0000

    User story 1: User Authentication, Scenarios 1.1, 1.2, 1.3

---
+++ SwagLabsFramework/BDD/Signin.feature
@@ -0,0 +1,28 @@
+﻿Feature: Signin
+	As a user of the sauce demo website
+	I want to be able to log in and out of my account
+	So I can access the product store
+
+@mytag
+Scenario: Valid Username/Password
+	Given I am on the signin page
+	And I have entered the username "standard_user"
+	And I have entered the password "secret_sauce"
+	When I click the login button
+	Then I should land on the Products page
+
+@mytag
+Scenario: Valid Username/Invalid Password
+	Given I am on the signin page
+	And I have entered the username "standard_user"
+	And I have entered the password "secretsauce"
+	When I click the login button
+	Then I should see an alert saying "Username and password do not match"
+
+@mytag
+Scenario: Invalid Username/Valid Password
+	Given I am on the signin page
+	And I have entered the username "user"
+	And I have entered the password "secret_sauce"
+	When I click the login button
+	Then I should see an alert saying "Username and password do not match"
---
+++ SwagLabsFramework/BDD/SigninSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace SwagLabsFramework
+{
+    [Binding]
+    public class SigninSteps
+    {
+        private SwagLabs _swagLabs = new SwagLabs();
+
+        [Given(@"I am on the signin page")]
+        public void GivenIAmOnTheSigninPage()
+        {
+            _swagLabs.SigninPage.VisitSigninPage();
+        }
+        
+        [Given(@"I have entered the username ""(.*)""")]
+        public void GivenIHaveEnteredTheUsername(string username)
+        {
+            _swagLabs.SigninPage.InputUsername(username);
+        }
+        
+        [Given(@"I have entered the password ""(.*)""")]
+        public void GivenIHaveEnteredThePassword(string password)
+        {
+            _swagLabs.SigninPage.InputPassword(password);
+        }
+        
+        [When(@"I click the login button")]
+        public void WhenIClickTheLoginButton()
+        {
+            _swagLabs.SigninPage.ClickLogin();
+        }
+        
+        [Then(@"I should land on the Products page")]
+        public void ThenIShouldLandOnTheProductsPage()
+        {
+            Assert.That(_swagLabs.SeleniumDriver.Url, Is.EqualTo("https://www.saucedemo.com/inventory.html"));
+        }
+
+        [Then(@"I should see an alert saying ""(.*)""")]
+        public void ThenIShouldSeeAnAlertSaying(string expected)
+        {
+            Assert.That(_swagLabs.SigninPage.GetErrorMessage(), Does.Contain(expected));
+        }
+
+        [AfterScenario]
+        public void DisposeWebDriver()
+        {
+            _swagLabs.SeleniumDriver.Quit();
+            _swagLabs.SeleniumDriver.Dispose();
+        }
+    }
+}

8fd6e76a4996e6470b1a8384f967e491fbc8ea19
KSohal <kavannn@live.com>
2021-01-21 12:11:39 +0000

    Added scenarios and steps 2.1, 2.2, 2.3 and 2.4. Added methods to CartPage.cs and ProductsPage.cs.

--- SwagLabsFramework/lib/pages/ProductsPage.cs
+++ SwagLabsFramework/lib/pages/ProductsPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using OpenQA.Selenium;
+using System.Threading;
 
 namespace SwagLabsFramework
 {
@@ -7,9 +8,42 @@ namespace SwagLabsFramework
     {
         private IWebDriver _seleniumDriver;
 
+        private string ProductsPageUrl = AppConfigReader.ProductsPageUrl;
+        private IWebElement _cart => _seleniumDriver.FindElement(By.CssSelector("path"));
+        private IWebElement _cartCount => _seleniumDriver.FindElement(By.CssSelector(".fa-layers-counter"));
+
         public ProductsPage(IWebDriver seleniumDriver)
         {
             _seleniumDriver = seleniumDriver;
         }
+
+        public void VisitProductPage()
+        {
+            _seleniumDriver.Navigate().GoToUrl(ProductsPageUrl);
+        }
+
+        public void AddToCart(string item_no)
+        {
+            _seleniumDriver.FindElement(By.CssSelector($".inventory_item:nth-child({item_no}) .btn_primary")).Click();
+        }
+        public void RemoveFromCart(string item_no)
+        {
+            _seleniumDriver.FindElement(By.CssSelector($".inventory_item:nth-child({item_no}) .btn_secondary")).Click();
+        }
+
+        public void ClickOnCart()
+        {
+            _cart.Click();
+        }
+
+        public string CartCount()
+		{
+            return _cartCount.Text;
+		}
+
+        public string Url()
+		{
+            return ProductsPageUrl;
+		}
     }
 }
